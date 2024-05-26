806a4fb46faea153deca6a91b2f06f1670d95cbe
菅田理央 <riosugata05@gmail.com>
2024-05-27 08:18:51 +0900

    added react files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import TodoList from "./TodoList";
+
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, name: "Todo1", completed: false },
+  ]);
+
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </>
+  );
+}
+
+export default App;
